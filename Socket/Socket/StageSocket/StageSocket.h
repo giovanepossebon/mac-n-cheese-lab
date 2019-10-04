82b6388c71fe2156c950c05fc4074ab4c01e0097
Rafael Leandro <rafaelleandro@me.com>
2019-10-04 16:45:56 -0300

    Add socket framework

---
+++ Socket/Socket/StageSocket/StageSocket.h
@@ -0,0 +1,18 @@
+//
+//  StageSocket.h
+//  StageSocket
+//
+//  Created by Rafael Leandro on 10/4/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StageSocket.
+FOUNDATION_EXPORT double StageSocketVersionNumber;
+
+//! Project version string for StageSocket.
+FOUNDATION_EXPORT const unsigned char StageSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StageSocket/PublicHeader.h>
+
+

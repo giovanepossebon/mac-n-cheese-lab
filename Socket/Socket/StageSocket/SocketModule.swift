29b48298dc4ca1986851c2f2ad145abdf9c24eee
Rafael Leandro <rafaelleandro@me.com>
2019-10-04 16:58:24 -0300

    Add Podfile

---
+++ Socket/Socket/StageSocket/SocketModule.swift
@@ -0,0 +1,15 @@
+//
+//  SocketModule.swift
+//  StageSocket
+//
+//  Created by Rafael Leandro on 10/4/19.
+//
+
+import Foundation
+import SocketIO
+
+final class SocketModule {
+    
+    
+    
+}
